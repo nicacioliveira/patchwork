6322ab96ab1b6cc804cb86144c3be449613e5a48
nicacioliveira <nicacio.sousa@ccc.ufcg.edu.br>
2016-11-16 22:47:42 -0300

    Classe pessoa modificada

--- Pessoa.java
+++ Pessoa.java
@@ -4,16 +4,21 @@ public class Pessoa {
 	private String sobrenome;
 
 	public Pessoa(String nome, String sobrenome) {
-		this.nome = nome;
-		this.sobrenome = sobrenome;
+		this.setNome(nome);
+		this.setSobrenome(sobrenome)
 	}
 
-	public void setNome(String nome) {
+	public void setNome(String nome) throws IllegalArgumentException {
+		if (nome.isEmpty)
+			throw new IllegalArgumentException("Entrada vazia!!!");
 		this.nome = nome;
 	}
 
-	public void setSobrenome(String sobrenome) {
+	public void setSobrenome(String sobrenome) throws IllegalArgumentException{
+		if (sobrenome.isEmpty)
+			throw new IllegalArgumentException("Entrada vazia!!!");
 		this.sobrenome = sobrenome;
+
 	}
 
 
