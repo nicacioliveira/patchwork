a7821d0b13be9fbb9dd729c7876208699e5635fe
nicacioliveira <nicacio.sousa@ccc.ufcg.edu.br>
2016-11-16 23:02:42 -0300

    idade e sexo adicionados a classe pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -2,10 +2,14 @@ public class Pessoa {
 
 	private String nome;
 	private String sobrenome;
+	private int idade;
+	private String sexo;
 
-	public Pessoa(String nome, String sobrenome) {
+	public Pessoa(String nome, String sobrenome, int idade, String sexo) {
 		this.nome = nome;
+		this.sexo = sexo;
 		this.sobrenome = sobrenome;
+		this.idade = idade;
 	}
 
 	public void setNome(String nome) {
