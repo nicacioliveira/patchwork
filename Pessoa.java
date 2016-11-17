cc3eb85357631988d33d284925b8bd4f01137718
nicacioliveira <nicacio.sousa@ccc.ufcg.edu.br>
2016-11-16 22:56:46 -0300

    Classe Pessoa adicionada

---
+++ Pessoa.java
@@ -0,0 +1,18 @@
+public class Pessoa {
+
+	private String nome;
+	private String sobrenome;
+
+	public Pessoa(String nome, String sobrenome) {
+		this.nome = nome;
+		this.sobrenome = sobrenome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setSobrenome(String sobrenome) {
+		this.nome = nome;
+	}
+}
